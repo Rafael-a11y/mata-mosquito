97d934d6e797688d519f8979a7fdd94705f5096d
Rafael Souto da Silva <rsowtto@gmail.com>
2023-06-12 23:56:05 -0300

    O game aplica mais de um mosquito na tela, e a ordem em que as moscas se movem é aleatória, antes as moscas eram geradas na tela e a primeira mosca a ser gerada sempre se movia primeiro, depois a segunda mosca gerada se movia para algum ponto da tela, e a terceira mosca se movia para algum ponto aleatório na tela, a mesma lógica acontecia numa lista maior de moscas, como um grupo de 4 ou 5 ou n moscas, a ordem era sempre a mesma, as moscas se moviam na ordem em que foram renderizadas na tela, ou seja, a mosca movida tinha que esperar as outras se moverem para depois chegar a sua vez de se mover

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 setInterval(function(){executarProjeto();}, 1000);
+
 let contador = 0;
 let trilhaSonora = document.getElementById("trilha-sonora");
 let audioPontuacao =  document.getElementById("pontuou");
@@ -7,15 +8,23 @@ let audioErrou = document.getElementById("errou");
 function executarProjeto()
 {
 
-    if($("#mosquito")) $("#mosquito").remove();
+    if(document.getElementsByClassName("mosquito") && document.getElementsByClassName("mosquito").length > 2)
+    {
+        var lista = [...document.getElementsByClassName("mosquito")];
+        let indice = Math.floor(Math.random() * (3 - 0) + 0);
+        console.log(indice);
+        lista[indice].remove();
+           
+    }
 
     let altura = window.innerHeight;
     let largura = window.innerWidth;
     let posicaoAltura = Math.abs(Math.random() * altura - 100);
     let posicaoLargura = Math.abs(Math.random() * largura  - 100);
-    let imagem = document.createElement("img");
+    let imagem = document.createElement("img"); 
 
     imagem.setAttribute("src", "./assets/mosquito.png");
+    imagem.setAttribute("data-click", "clicou");
     imagem.id = "mosquito";
     imagem.className = "mosquito " + mudarTamanho();
     imagem.style.top = posicaoAltura + "px";
@@ -24,14 +33,23 @@ function executarProjeto()
     document.body.appendChild(imagem);
     document.body.onclick = function(evento)
     {
+        let condicao = (mosca) =>
+            ((evento.clientX > mosca.x) && (evento.clientX < (mosca.x + mosca.width)))
+            && 
+            ((evento.clientY > mosca.y) && (evento.clientY < (mosca.y + mosca.height)));
+
+        let clique = (mosca) => mosca.setAttribute("data-click", "clicou");
+
+        /*Converte o HTMLCollection<Element> retornado por getElementsByClassName("mosquito") em um array de
+            Element, ou seja: Element[]*/
+        let array = [...document.getElementsByClassName("mosquito")];
+
         trilhaSonora.play();
         if
-        (
-            ((evento.clientX > imagem.x) && (evento.clientX < (imagem.x + imagem.width))) 
-            && 
-            (evento.clientY > (imagem.y) && (evento.clientY < (imagem.y + imagem.height)))
-        )
+        (array && array.some(condicao))
         {   
+            let clicado =  array.filter(condicao);
+            clicado[0].style.backgroundColor = "red";
             contador++;
             // imagem.src = "./assets/sangue2.png";
             audioPontuacao.play();
@@ -44,7 +62,7 @@ function executarProjeto()
                 audioErrou.play();
             } 
         }
-        $("#contador").html(contador);
+        document.getElementById("contador").textContent = contador;
     }
 }
 
@@ -53,7 +71,7 @@ function executarProjeto()
 function mudarTamanho()
 {
     let classe = Math.floor(Math.random() * 3);
-    if(classe == 0) return "tamanho1";
-    else if(classe == 1 ) return "tamanho2";
-    else return "tamanho3";
+    if(classe == 0) return "tamanho1"; //Se 0 - tamanho1
+    else if(classe == 1 ) return "tamanho2"; //Se 1 - tamanho2
+    else return "tamanho3"; //Se 2 - tamanho3
 }
