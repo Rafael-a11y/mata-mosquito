9494ad1a5346fb278dfc6fd58c92a9e323e554c1
Rafael Souto da Silva <rsowtto@gmail.com>
2023-06-13 11:02:00 -0300

    Label e as moscas não são selecionáveis, desativei também o movimento de clique e earrasta do mouse em cima das mocas, caso o usuário seja rápido a e apenas uma mosca sobre e essa também seja removida pelo clique do usuário, outras moscas são renderizadas de uma só vez na tela para não permitir que apenas uma mosca fique sendo renderizada

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-setInterval(function(){executarProjeto();}, 1000);
 
 let contador = 0;
-let trilhaSonora = document.getElementById("trilha-sonora");
+let trilhaSonora =  document.getElementById("trilha-sonora");
 let audioPontuacao =  document.getElementById("pontuou");
 let audioErrou = document.getElementById("errou");
 
-function executarProjeto()
+
+setInterval(function(){executarProjeto();}, 1000);
+
+//Gera aleatóriamente uma string representando uma classe, pode retornar tamanho1, tamanho2 ou tamanho3.
+function mudarTamanho()
 {
+    let classe = Math.floor(Math.random() * 3);
+    if(classe == 0) return "tamanho1"; //Se 0 - tamanho1
+    else if(classe == 1 ) return "tamanho2"; //Se 1 - tamanho2
+    else return "tamanho3"; //Se 2 - tamanho3
+}
 
-    if(document.getElementsByClassName("mosquito") && document.getElementsByClassName("mosquito").length > 2)
-    {
-        //O número gerado para índice será sempre de min até max, já que o valor limite max é exclusivo.
-        var lista = [...document.getElementsByClassName("mosquito")];
-        let indice = Math.floor(Math.random() * (3 - 0) + 0);
-        lista[indice].remove();
-           
-    }
-
+function gerarMosquito()
+{
     let altura = window.innerHeight;
     let largura = window.innerWidth;
     let posicaoAltura = Math.abs(Math.random() * altura - 100);
@@ -29,8 +30,32 @@ function executarProjeto()
     imagem.className = "mosquito " + mudarTamanho();
     imagem.style.top = posicaoAltura + "px";
     imagem.style.left = posicaoLargura + "px";
+    imagem.addEventListener("dragstart", function (evento)
+    {
+        evento.preventDefault();
+    });
 
     document.body.appendChild(imagem);
+}
+
+function removerMosquito()
+{
+    if(document.getElementsByClassName("mosquito") && document.getElementsByClassName("mosquito").length > 2)
+    {
+        //O número gerado para índice será sempre de min até max, já que o valor limite max é exclusivo.
+        var lista = [...document.getElementsByClassName("mosquito")];
+        let indice = Math.floor(Math.random() * (3 - 0) + 0);
+        lista[indice].remove();
+           
+    }
+}
+
+function executarProjeto()
+{
+    trilhaSonora.play();
+    removerMosquito();
+    gerarMosquito();
+    
     document.body.onclick = function(evento)
     {
         let condicao = (mosca) =>
@@ -44,33 +69,32 @@ function executarProjeto()
             Element, ou seja: Element[]*/
         let array = [...document.getElementsByClassName("mosquito")];
 
-        trilhaSonora.play();
+        //trilhaSonora.play();
         if
         (array && array.some(condicao))
         {   
-            let clicado =  array.filter(condicao);
+            let clicado =  array.some(condicao)? array.filter(condicao) : null;
             clicado[0].remove();
+            console.log("Length após o clique: " + array.length);
             contador++;
-            // imagem.src = "./assets/sangue2.png";
+            audioPontuacao.currentTime = 0;
             audioPontuacao.play();
+            if(array.length == 1)
+            {
+                gerarMosquito();
+                gerarMosquito();
+                gerarMosquito();
+            }
         }
         else
         {
             if(contador > 0)
             {
                 contador--;
+                audioErrou.currentTime = 0;
                 audioErrou.play();
             } 
         }
         document.getElementById("contador").textContent = contador;
     }
-}
-
-//Gera aleatóriamente uma string representando uma classe, pode retornar tamanho1, tamanho2 ou tamanho3.
-function mudarTamanho()
-{
-    let classe = Math.floor(Math.random() * 3);
-    if(classe == 0) return "tamanho1"; //Se 0 - tamanho1
-    else if(classe == 1 ) return "tamanho2"; //Se 1 - tamanho2
-    else return "tamanho3"; //Se 2 - tamanho3
 }
